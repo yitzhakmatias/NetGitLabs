84d142bd4dbf652be32e7f161a446838b7077126
yitzhak matias <matias6@gmail.com>
2016-05-05 15:15:30 -0400

    Added from vs

--- GitTestProject/GitTestProject/Program.cs
+++ GitTestProject/GitTestProject/Program.cs
@@ -13,6 +13,7 @@ namespace GitTestProject
             //******
             //** Changes on the main program test
             // changes from  client site
+            // Test changing from git test
             //****
         }
     }
